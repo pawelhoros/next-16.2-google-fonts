a3c3204514e68c861d0517d9f779c19552c2e204
Paweł Horoś <pawel.horos@are.waw.pl>
2026-03-19 14:54:59 +0100

    google fonts in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,22 @@
+import { Geist, Geist_Mono } from 'next/font/google';
+
+const fontSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+const fontMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
   );
